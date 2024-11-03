d9007782d2a6e77a4a01a7f579f8e2bb93337c46
Eleaza-R <otoeleazarivas@gmail.com>
2024-11-03 13:52:49 -0600

    funcionalidad de los checbox

--- codigo.js
+++ codigo.js
@@ -1,66 +1,75 @@
-var fila = "<tr><td class='id'></td><td class='foto'></td><td class='price'></td><td class='title'></td><td class='description'></td><td class='category'></td></tr>";
-var productos = null;
-var orden = 0;
-
-function codigoCat(catstr) {
-  switch (catstr) {
-    case "electronics": return "c1";
-    case "jewelery": return "c2";
-    case "men's clothing": return "c3";
-    case "women's clothing": return "c4";
-    default: return "null";
-  }
-}
-
-function listarProductos(productos) {
-  var precio = document.getElementById("price");
-  precio.setAttribute("onclick", "orden*=-1;listarProductos(productos);");
-  var tbody = document.getElementById("tbody");
-  tbody.innerHTML = "";
-
-  const categoriasSeleccionadas = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(input => input.value);
-  const productosFiltrados = productos.filter(producto => categoriasSeleccionadas.includes(codigoCat(producto.category)));
-
-  if (orden === 1) ordenarAsc(productosFiltrados, "price");
-  else if (orden === -1) ordenarDesc(productosFiltrados, "price");
-
-  productosFiltrados.forEach((producto, index) => {
-    tbody.innerHTML += fila;
-    tbody.querySelectorAll(".id")[index].textContent = producto.id;
-    tbody.querySelectorAll(".title")[index].textContent = producto.title;
-    tbody.querySelectorAll(".description")[index].textContent = producto.description;
-    tbody.querySelectorAll(".category")[index].textContent = producto.category;
-    tbody.querySelectorAll(".price")[index].textContent = "$" + producto.price;
-    tbody.querySelectorAll(".foto")[index].innerHTML = `<img src="${producto.image}" onclick="window.open('${producto.image}');">`;
-  });
-
-  document.getElementById("listado").style.display = "block";
-  precio.textContent = orden === 1 ? "Precio Asc" : "Precio Desc";
-
-  // Agregar datos "Guía #61b" y "RN22001" al listado
-  document.querySelector(".container").insertAdjacentHTML('beforeend', `
-    <p>Guía #61b</p>
-    <p>RN22001</p>
-  `);
-}
+var fila="<tr><td class='id'></td><td class='foto'></td><td class='price'></td><td class='title'></td><td class='description'></td><td class='category'></td></tr>";
+	 var productos=null;
+  function codigoCat(catstr) {
+	var code="null";
+	switch(catstr) {
+		case "electronics":code="c1";break;
+	    case "jewelery":code="c2";break;
+		case "men's clothing":code="c3";break;
+		case "women's clothing":code="c4";break;
+	}
+	return code;
+}   
+	  var orden=0;
+	  
+	  
+	function listarProductos(productos) {
+	  var precio=document.getElementById("price"); 
+	  precio.setAttribute("onclick", "orden*=-1;listarProductos(productos);");
+	  var num=productos.length;
+	  var listado=document.getElementById("listado");
+	  var ids,titles,prices,descriptions,categories,fotos;
+	  var tbody=document.getElementById("tbody"),nfila=0;
+	  tbody.innerHTML="";
+	  var catcode;
+	  for(i=0;i<num;i++) tbody.innerHTML+=fila;
+	  var tr; 
+	  ids=document.getElementsByClassName("id");
+	  titles=document.getElementsByClassName("title");
+	  descriptions=document.getElementsByClassName("description");
+	  categories=document.getElementsByClassName("category");   
+	  fotos=document.getElementsByClassName("foto");   
+	  prices=document.getElementsByClassName("price");   
+	  if(orden===0) {orden=-1;precio.innerHTML="Precio"}
+	  else
+	     if(orden==1) {ordenarAsc(productos,"price");precio.innerHTML="Precio A";precio.style.color="darkgreen"}
+	     else 
+	       if(orden==-1) {ordenarDesc(productos,"price");precio.innerHTML="Precio D";precio.style.color="blue"}
+	
+		  
+	  	  listado.style.display="block";
+	  for(nfila=0;nfila<num;nfila++) {
+        ids[nfila].innerHTML=productos[nfila].id;
+		titles[nfila].innerHTML=productos[nfila].title;
+		descriptions[nfila].innerHTML=productos[nfila].description;
+		categories[nfila].innerHTML=productos[nfila].category;
+		catcode=codigoCat(productos[nfila].category);
+		tr=categories[nfila].parentElement;
+		tr.setAttribute("class",catcode);
+		prices[nfila].innerHTML="$"+productos[nfila].price;
+		fotos[nfila].innerHTML="<img src='"+productos[nfila].image+"'>";
+		fotos[nfila].firstChild.setAttribute("onclick","window.open('"+productos[nfila].image+"');" );
+		}
+	}
 
 function obtenerProductos() {
-  fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(data => {
-      productos = data;
-      listarProductos(data);
-    });
+	  fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=>{productos=data;listarProductos(data)})
 }
 
-function ordenarDesc(arr, key) {
-  arr.sort((a, b) => b[key] - a[key]);
+function ordenarDesc(p_array_json, p_key) {
+   p_array_json.sort(function (a, b) {
+      if(a[p_key] > b[p_key]) return -1;
+if(a[p_key] < b[p_key]) return 1;
+return 0;
+   });
 }
 
-function ordenarAsc(arr, key) {
-  arr.sort((a, b) => a[key] - b[key]);
+function ordenarAsc(p_array_json, p_key) {
+   p_array_json.sort(function (a, b) {
+      if(a[p_key] > b[p_key]) return 1;
+if(a[p_key] < b[p_key]) return -1;
+return 0;
+   });
 }
-
-document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
-  checkbox.addEventListener("change", () => listarProductos(productos));
-});
